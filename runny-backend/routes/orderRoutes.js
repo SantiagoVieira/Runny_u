3c6652687a9d7e08fce2fd4cdcfe6d90a41c4c3d
MiguelHerazo7 <98978366+MiguelHerazo@users.noreply.github.com>
2024-10-20 13:50:36 -0500

    Update orderRoutes.js

--- runny-backend/routes/orderRoutes.js
+++ runny-backend/routes/orderRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const {getHistoryByUser} = require('../controllers/orderController')
 
-router.get('/history', getHistoryByUser);
-=======
 const orderController = require('../controllers/orderController');
 
 router.post('/', orderController.createOrder); // Creamos la orden
@@ -12,6 +8,6 @@ router.get('/:orderId', orderController.getOrderStatus); // Obtenenemos el estad
 router.get('/history', orderController.getOrderHistory); // Obtenemos el historial de las ordenes
 router.post('/delivery-option', orderController.selectDeliveryOption); // Seleccionamos uba opcinn de entrega
 router.post('/delivery-location', orderController.setDeliveryLocation); // Establecemos la ubicacion de la entrega
->>>>>>> c031fc2b4f9229f53424ff045a4ea174bd7d2847
+
 
 module.exports = router;
