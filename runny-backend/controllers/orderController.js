eb33ce547f16ef80764e47cf8c486eab81f68fef
MiguelHerazo <herazomigue56@gmail.com>
2024-10-20 13:49:36 -0500

    Implementado el historial ahora si bien

--- runny-backend/controllers/orderController.js
+++ runny-backend/controllers/orderController.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Order = require('../models/Order');
 
 exports.getHistoryByUser = async (req, res) => {
@@ -21,33 +21,4 @@ exports.getHistoryByUser = async (req, res) => {
     console.error(error);
     res.status(500).json({ message: 'Error buscando el historial de compras' });
   }
-=======
-// orderController.js
-let orderHistory = [];
-
-exports.createOrder = (req, res) => {
-    const { items, storeId } = req.body; 
-    const orderId = `order-${Date.now()}`; 
-    orderHistory.push({ orderId, items, storeId });
-    res.status(201).json({ message: 'orden dreada exitosamente', orderId });
-};
-
-exports.getOrderStatus = (req, res) => {
-    const orderId = req.params.orderId;
-    res.status(200).json({ orderId, status: 'en preparacion' });
-};
-
-exports.getOrderHistory = (req, res) => {
-    res.status(200).json(orderHistory);
-};
-
-exports.selectDeliveryOption = (req, res) => {
-    const { option } = req.body; 
-    res.json({ message: `Opcion de entrega seleccionada: ${option}` });
-};
-
-exports.setDeliveryLocation = (req, res) => {
-    const { location } = req.body; 
-    res.json({ message: 'Ubicación de entrega establecida.', location });
->>>>>>> c031fc2b4f9229f53424ff045a4ea174bd7d2847
-};
+}
