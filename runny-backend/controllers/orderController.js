0a96181a8e4d28780b8ae7a9cfabd308fb059f4b
MiguelHerazo7 <98978366+MiguelHerazo@users.noreply.github.com>
2024-10-20 13:51:22 -0500

    Update orderController.js

--- runny-backend/controllers/orderController.js
+++ runny-backend/controllers/orderController.js
@@ -1,11 +1,32 @@
+// orderController.js
+let orderHistory = [];
 
-const Order = require('../models/Order');
+exports.createOrder = (req, res) => {
+    const { items, storeId } = req.body; 
+    const orderId = `order-${Date.now()}`; 
+    orderHistory.push({ orderId, items, storeId });
+    res.status(201).json({ message: 'orden dreada exitosamente', orderId });
+};
 
-exports.getHistoryByUser = async (req, res) => {
+exports.getOrderStatus = (req, res) => {
+    const orderId = req.params.orderId;
+    res.status(200).json({ orderId, status: 'en preparacion' });
+};
+
+exports.getOrderHistory = async (req, res) => { 
   try {
+    // Busca las órdenes del usuarion orndenado de mas reciente primero
     const orders = await Order.find({ id_usuario: req.user.id }).sort({ _id: -1 });
 
-    // Saca la info de cada pedido 
+    // Si no encuentra saca esto
+    if (orders.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: 'No se encontraron órdenes para este usuario'
+      });
+    }
+
+    // Devuelve la info de los pedido
     res.status(200).json({
       success: true,
       data: orders.map(order => ({
@@ -19,6 +40,18 @@ exports.getHistoryByUser = async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error buscando el historial de compras' });
+    res.status(500).json({ message: 'Error buscando el historial de compras', error });
   }
-}
+};
+
+
+exports.selectDeliveryOption = (req, res) => {
+    const { option } = req.body; 
+    res.json({ message: `Opcion de entrega seleccionada: ${option}` });
+};
+
+exports.setDeliveryLocation = (req, res) => {
+    const { location } = req.body; 
+    res.json({ message: 'Ubicación de entrega establecida.', location });
+
+};
